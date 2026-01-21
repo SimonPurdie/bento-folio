958011a0538a6f1d5733ba783f87aca75f1b10fe
SimonPurdie <hoopmonkey@gmail.com>
2026-01-21 12:45:58 +0000

    chore: add deployment guide and configure paths for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://SimonPurdie.github.io',
+  base: '/bento-folio',
   vite: {
     plugins: [tailwindcss()]
   }
